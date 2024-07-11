adec24929b660b950f7aa20fd597792f5b155cfe
Caesarblack27 <caesargattacaa11@gmail.com>
2024-07-12 03:24:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,27 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from tensorflow.keras.models import load_model
 import requests
-from io import BytesIO  # Perhatikan penambahan ini
+import tempfile
+import os
 
 # Function to load the pretrained model
 @st.cache(allow_output_mutation=True)
 def load_pretrained_model():
     try:
-        # Load model directly from GitHub URL
+        # Load model from GitHub
         model_url = "https://github.com/Caesarblack27/rain-prediction/raw/main/rain_prediction_model.h5"
         response = requests.get(model_url)
         response.raise_for_status()
         
-        # Load model directly from content
-        model_content = BytesIO(response.content)  # Simpan konten sebagai BytesIO
-        model = load_model(model_content, compile=False)  # Gunakan model_content sebagai objek BytesIO
+        # Save model to a temporary file
+        with tempfile.NamedTemporaryFile(delete=False, suffix='.h5') as temp_model:
+            temp_model.write(response.content)
+            temp_model.close()
+            model = load_model(temp_model.name)
+        
+        # Delete temporary file after loading model
+        os.remove(temp_model.name)
+        
         return model
     except Exception as e:
         st.error(f"Unable to load model: {str(e)}")
