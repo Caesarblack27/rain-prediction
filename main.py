9b0bca30cd261c47a620b4a31ab2a203ea68ec8f
Caesarblack27 <caesargattacaa11@gmail.com>
2024-07-12 01:38:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,17 @@ import streamlit as st
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from tensorflow.keras.models import load_model
-from io import BytesIO
 import requests
-import numpy as np
 
 # Function to load the pretrained model
 @st.cache(allow_output_mutation=True)
 def load_pretrained_model():
-    # Load model from GitHub
-    model_url = "https://github.com/Caesarblack27/rain-prediction/raw/main/rain_prediction_model.h5"
-    response = requests.get(model_url)
-    response.raise_for_status()
-    
     try:
+        # Load model directly from GitHub URL
+        model_url = "https://github.com/Caesarblack27/rain-prediction/raw/main/rain_prediction_model.h5"
+        response = requests.get(model_url)
+        response.raise_for_status()
+        
         # Load model directly from content
         model = load_model(BytesIO(response.content))
         return model
@@ -69,7 +67,7 @@ def main():
         encoded_wind_gust_dir = label_encoder_wind_gust_dir.transform([wind_gust_dir])[0]
 
         # Prepare user data for prediction
-        user_data = np.array([[encoded_location, min_temp, max_temp, encoded_wind_gust_dir, wind_gust_speed]])
+        user_data = [[encoded_location, min_temp, max_temp, encoded_wind_gust_dir, wind_gust_speed]]
 
         # Scale user data
         user_data_scaled = scaler.transform(user_data)
