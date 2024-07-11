f9e330b519c18fcf06086fcb29abee2d174905ad
Caesarblack27 <caesargattacaa11@gmail.com>
2024-07-12 01:23:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,26 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler
 from keras.models import load_model
 from io import BytesIO
 import requests
+import tempfile
+import os
 
 # Function to load the pretrained model
 @st.cache(allow_output_mutation=True)
 def load_pretrained_model():
+    # Load model from GitHub
     model_url = "https://github.com/Caesarblack27/rain-prediction/raw/main/rain_prediction_model.h5"
     response = requests.get(model_url)
     response.raise_for_status()
-    model = load_model(BytesIO(response.content))
+    
+    # Save model to a temporary file
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.h5') as temp_model:
+        temp_model.write(response.content)
+        temp_model.close()
+        model = load_model(temp_model.name)
+    
+    # Delete temporary file after loading model
+    os.remove(temp_model.name)
+    
     return model
 
 # Load data from URL
